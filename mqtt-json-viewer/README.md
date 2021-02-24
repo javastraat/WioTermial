965961f5d5e46fa0828aa0617d87927a49585dfd
Albert Einstein <javastraat@hotmail.com>
2021-02-24 16:08:13 +0100

    <initial upload>

---
+++ mqtt-json-viewer/README.md
@@ -0,0 +1,57 @@
+# Wio Terminal MQTT Json viewer	
+<center>	
+Install Mosquitto MQTT Broker/Server if not done before.<br>	
+http://www.steves-internet-guide.com/install-mosquitto-linux/<br>	
+<br><br>	
+Booting MQT-Json-Viewer<br>	
+<img src="pictures/start.jpeg" width="300">	
+<br><br>	
+Simple display usage : <br>	
+Use Wio Terminal to see MQTT Data <br>	
+- our data is made by monitoring mmdvmhost logfile.<br>	
+<img src="pictures/simple.jpeg" width="300">	
+<br><br>	
+Basic display usage :<br>	
+Use Wio Terminal to see lastheard data (mqtt-json) from radioamateur Brandmeister network.<br>	
+-<b>This needs monitor and mqttbridge service and a running MMDVMHost instance (Hotspot, Bridge etc)</b><br>	
+-Getting notified when friend is talking on network (text turns red and wio terminal beeps two times).<br>	
+<img src="pictures/basic.jpeg" width="300">	
+<br><br>	
+Extended display usage (display all data):<br>	
+<img src="pictures/extended.jpeg" width="300"><br>	
+<br><br>	
+Domoticz data display (mqtt-json).<br>	
+-<b>This needs domoticz with mqtt plugin to generate data on domoticz/out</b><br>	
+<img src="pictures/domoticz.jpeg" width="300"><br>	
+<hr>	
+Options during boot:<br>	
+-Button A (right):  Escape to ExtFlashLoader Menu	
+<br><br>	
+Options after full start:<br>	
+-Button A (right):  Escape to ExtFlash Menu<br>	
+-Button B (middle): nothing yet (planned to move domoticz here)<br>	
+-Button C (left):   Helppage (press again to exit)	
+<br><br>	
+-Joystick left : Basic MQTT (plain) output (default)<br>	
+-Joystick up   : Nice MQTT output (mqtt contains json output)<br>	
+-Joystick right: Extended MQTT output (mqtt contains json output) <br>	
+-Joystick down : Domoticz MQTT output (mqtt contains json output)	
+<br><br>	
+<hr>	
+Thanx to :<br><br>	
+Components added to code:<br>	
+-MQTT example<br>	
+https://github.com/salmanfarisvp/Wio-Terminal-MQTT<br>	
+<br>	
+-Json processing (JsonParserExample)<br>	
+https://arduinojson.org/v6/example/parser/<br>	
+<br>	
+-NTP Timeclient (for timestamps and clock)<br>	
+https://github.com/arduino-libraries/NTPClient/tree/master/examples/Advanced<br>	
+<br>	
+-ExtFlashLoader (for sd menu)<br>	
+https://github.com/ciniml/ExtFlashLoader<br>	
+<br>	
+-alot of code found in other samples,on github and google.<br>	
+<br>	
+if your name should go here -> tell me. (sorry i forgot you)
---
+++ mqtt-json-viewer/services/lh_mqtt_domoticz.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3	
+import paho.mqtt.client as mqtt	
+import json	
+import os	
+import time	
+import sys	
+import pathlib	
+import urllib.request	
+
+# Settings	
+dom_idx=455	
+#sub_mqtt="Home/snespot/#"	
+sub_mqtt="Home/MMDVMBridge/LastHeard"	
+pub_mqtt_1="domoticz/in"	
+pub_mqtt="Home/MMDVMBridge/data"	
+mqtt_server_ip="192.168.x.xxx"	
+#database='/opt/MMDVM_Bridge/DMRIds.dat'	
+#url='https://raw.githubusercontent.com/DMR-Database/database-beta/master/DMRIds.dat'	
+url='https://raw.githubusercontent.com/DMR-Database/database-beta/master/usr.bin'	
+dirdb=os.getcwd()	
+#database=dirdb+"/DMRIds.dat"	
+database=dirdb+"/usr.bin"	
+#print(dirdb)	
+#	
+#check for DMRIds.dat else download from github	
+#	
+file = pathlib.Path("usr.bin")	
+if file.exists ():	
+    print ("Database exists continuing")	
+else:	
+    print ("File not exist")	
+    print("Downloading DMRIds.dat from", url)	
+    urllib.request.urlretrieve(url,database)	
+    print()	
+    print("Please restart python script")	
+    exit()	
+#	
+#	
+#This is the Subscriber connect	
+def on_connect(client, userdata, flags, rc):	
+    print ("--------------------------")	
+    if rc==0:	
+     print("Connection sucessfull")	
+    sys.stdout.flush()	
+#    print("Connected with result code "+str(rc))	
+    print("MQTT Server IP :",mqtt_server_ip)	
+    sys.stdout.flush()	
+    print("Feed Subscribe :",sub_mqtt)	
+    sys.stdout.flush()	
+    print("Feed Publish   :",pub_mqtt)	
+    sys.stdout.flush()	
+    print("Domoticz IDX   :",dom_idx)	
+    sys.stdout.flush()	
+    print("DMRIds.dat     :",database)	
+    sys.stdout.flush()	
+    client.subscribe(sub_mqtt)	
+    print("--------------------------")	
+    sys.stdout.flush()	
+    print("waiting for new message...")	
+    sys.stdout.flush()	
+
+
+def on_message(client, userdata, message):	
+#    print("message received :",str(message.payload.decode("utf-8")),\	
+#          "topic",message.topic,"retained ",message.retain)	
+#    if message.retain==1:	
+#        print("This is a retained message")	
+#	
+
+    m=str(message.payload.decode("utf-8"))	
+#    n=({'command': 'udevice', 'idx' : (dom_idx), 'svalue' : (m)})	
+    d = json.dumps({'dmrid': '2041152', 'callsign': (m), 'name': 'Test', 'town': 'Amsterdam','county': 'NH', 'country': 'NL '})	
+
+
+#lastheard :  WB4FWQ	
+#send to mqtt :  {'command': 'udevice', 'idx': 455, 'svalue': 'WB4FWQ'}	
+#    print (type(m))	
+#    print (type(n))	
+#    print (type(d))	
+#    print ("Message received :",(m))	
+#    print ("Json data : ",(d))	
+#    print ("Send to",pub_mqtt,":",(d))	
+#    print ("Publishing...")	
+    searchfile = open(database, "r")	
+    for line in searchfile:	
+        if m in line:	
+#          dump=json.dumps({'command': 'udevice', 'idx' : (dom_idx), 'svalue' :(line)})	
+          dmrid, callsign, name, town, county, country = line.split(',')	
+          d = json.dumps({'dmrid': (dmrid), 'callsign': (m), 'name': (name), 'town': (town),'county': (county), 'country': (country)})	
+          n = (callsign)+" "+(name)	
+          dump=json.dumps({'command': 'udevice', 'idx' : (dom_idx), 'svalue' :(n)})	
+
+#          print(n)	
+#          print(dmrid) 	
+#          print(callsign) 	
+#          print(name) 	
+#          print(town) 	
+#          print(county) 	
+#          print(country)	
+
+
+
+##           print(line)	
+#          print ("Publishing...")	
+#          sys.stdout.flush()	
+    client.publish(pub_mqtt,d)	
+#    client.publish(pub_mqtt_1,dump)	
+    searchfile.close()	
+#    print ("--------------------------")	
+#    print ("waiting for new message...")	
+
+
+client = mqtt.Client()	
+client.connect(mqtt_server_ip,1883,60)	
+
+client.on_connect = on_connect	
+client.on_message = on_message	
+
+
+client.loop_forever()
